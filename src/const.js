9534f64a2f950e10ff80dfddc8dc825407851a0e
Andragon <oscarcamarena31@hotmail.com>
2021-05-19 13:19:15 -0600

    Initial commit

---
+++ src/const.js
@@ -0,0 +1,15 @@
+// put your own APP_ID here
+// get your app_id -> https://dashboard.sendbird.com/auth/signin
+export const APP_ID = "7AB76320-34EA-4CA7-BA05-A5303DFBAAD2";
+
+// set your own USER_ID and NICKNAME
+export const USER_ID = "Adra-2021";
+export const NICKNAME = "Adra";
+export const THEME = "light" || "dark";
+
+export default {
+  APP_ID,
+  USER_ID,
+  NICKNAME,
+  THEME
+};
